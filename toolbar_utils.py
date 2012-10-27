eb8aee3242a4165ef04f3ddc8eb3f4c4a39e439c
Walter Bender <walter.bender@gmail.com>
2012-10-27 09:27:39 -0400

    clean up

--- toolbar_utils.py
+++ toolbar_utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2011, Walter Bender
-# Copyright (c) 2012, Ignacio Rodriguez
+# Port To GTK3:
+# Ignacio Rodriguez <ignaciorodriguez@sugarlabs.org>
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation; either version 3 of the License, or
@@ -9,6 +10,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this library; if not, write to the Free Software
 # Foundation, 51 Franklin Street, Suite 500 Boston, MA 02110-1335 USA
+
 
 from gi.repository import Gtk
 
